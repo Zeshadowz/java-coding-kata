a029e929f68ca6307068ad0ec209804ae6f2facb
Kevin Azangue <kevinromuald@gmail.com>
2024-05-05 11:22:53 +0200

    clean code

--- spring-boot-module/db-mysql/src/main/java/com/qburry/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package com.qburry.repository;
-
-import com.qburry.model.PersonEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonRepository extends JpaRepository<PersonEntity, Long> {
-}
